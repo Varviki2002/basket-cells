d40ac1322c7045a71d66f1e83f1a3f88a7ce7fd3
Viki <viktoria.varkonyi02@gmail.com>
2023-08-07 14:45:20 +0200

    Update files

--- src/plot.py
+++ src/plot.py
@@ -8,7 +8,7 @@ from src.datamanipulator import DataManipulator
 class Plotter(DataManipulator):
     def __init__(self, data):
         self.data = data
-        self.dict = DataManipulator.create_dict(all_in_one=False)
+        self.dict = DataManipulator.create_dict(self, all_in_one=False)
         self.names = [x[0] for x in self.data.columns][::2]
         super().__init__(data)
 
