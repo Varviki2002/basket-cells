ddefbd01588d250e01c2fb2d3117610459ee4c6f
Viki <viktoria.varkonyi02@gmail.com>
2023-08-08 13:05:30 +0200

    Update files

--- src/plot.py
+++ src/plot.py
@@ -12,9 +12,14 @@ class Plotter:
         self.dict = data_class.dict
         self.names = data_class.names
 
-    def plot_spike(self, name: str, spike_name: str, color: str) -> None:
-        plt.scatter(self.dict[name][spike_name]["relative firing time"],
-                    self.dict[name][spike_name]["IF"], c=color)
+    def plot_spike(self, name: str, spike_name: str, color: str, all: bool) -> None:
+        if all:
+            all_dict = self.data_class.create_dict(all_in_one=True)
+            plt.scatter(all_dict[spike_name]["relative firing time"],
+                        all_dict[spike_name]["IF"], c=color)
+        else:
+            plt.scatter(self.dict[name][spike_name]["relative firing time"],
+                        self.dict[name][spike_name]["IF"], c=color)
         plt.title(name)
         plt.xlabel("relative firing time")
         plt.ylabel("IF")
@@ -25,7 +30,7 @@ class Plotter:
                  for i in range(len(self.dict[cell_name]))]
         for i in range(1, len(self.dict[cell_name]) + 1):
             spike = f"{i}.spike"
-            self.plot_spike(name=cell_name, spike_name=spike, color=color[i - 1])
+            self.plot_spike(name=cell_name, spike_name=spike, color=color[i - 1], all=False)
 
     def plot_frame_spike(self, name: str, spike_frame: pd.DataFrame, color: str) -> None:
         plt.scatter(spike_frame["relative firing time"], spike_frame["IF"],
