08add451f1ad83dc829caddc8f594de6d1ef6aaa
Viki <viktoria.varkonyi02@gmail.com>
2023-10-05 01:06:36 +0200

    Refactor the files

--- src/evaluate.py
+++ src/evaluate.py
@@ -44,8 +44,9 @@ class Evaluate:
             final = func_class(params=result.params, x=df["relative firing time"])
             mean = np.mean(df["IF"])
             r_2 = (np.sum((mean-df["IF"]) ** 2) - np.sum((df["IF"] - final) ** 2)) / np.sum((mean-df["IF"]) ** 2)
-            p, r_square, conf_int, fp, f, params = self.linear_regression(x=df["relative firing time"], y=df["IF"])
+            p, r_square, conf_int, fp, f, params, chisqr = self.linear_regression(x=df["relative firing time"], y=df["IF"])
             self.linear_regression_parameters[cell_name][spike_name][round(10 ** num)]["p"] = p
+            self.linear_regression_parameters[cell_name][spike_name][round(10 ** num)]["chisqr"] = chisqr
             self.linear_regression_parameters[cell_name][spike_name][round(10 ** num)]["r_square"] = r_square
             self.linear_regression_parameters[cell_name][spike_name][round(10 ** num)]["conf_int"] = conf_int
             self.linear_regression_parameters[cell_name][spike_name][round(10 ** num)]["r_2"] = r_2
@@ -66,7 +67,8 @@ class Evaluate:
         conf_int = result.conf_int(alpha=0.05, cols=None)
         fp = result.f_pvalue
         f = result.fvalue
-        return p, r_square, conf_int, fp, f, params
+        chisqr = result.chisqr
+        return p, r_square, conf_int, fp, f, params, chisqr
 
     def compare_values_to_original(self):
         pass
