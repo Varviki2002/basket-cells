03050736d4e7a9b1a05a8713e373ea4ab95ec786
Viki <viktoria.varkonyi02@gmail.com>
2023-10-05 22:51:49 +0200

    reformat

--- src/lm_fit.py
+++ src/lm_fit.py
@@ -94,12 +94,13 @@ class LMFit:
                     params=result.params,
                     x=x))
 
-            if show:
-                # report_fit(result)
                 df_n = self.show_the_fit_results(df=df_n, num_params=func_class.n_params, result=result, spike=spike)
                 df_params = self.show_the_param_results(df=df_params, num_params=func_class.n_params,
                                                         name_to_save=name_to_save, range_spike=range_spike,
                                                         do_all=do_all, cell_name=cell_name, spike=spike)
+
+            if show:
+                # report_fit(result)
                 # plot results
                 if log:
                     self.plotter.plot_fitted_data(x=x, data=data, final=final, log=log,
