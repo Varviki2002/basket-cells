c69c4a6101301a400edc6e77dc72d862d39223b1
Viki <viktoria.varkonyi02@gmail.com>
2023-10-06 23:16:48 +0200

    reformat

--- src/evaluate.py
+++ src/evaluate.py
@@ -42,8 +42,8 @@ class Evaluate:
             for i in range(0, len(dict_frame)):
                 if dict_frame["IF"].iloc[i] > num:
                     df = df.drop(labels=i, axis=0)
-            result = self.lm_fit.fit_the_function(func_class=func_class, param_values=param_values,
-                                                  x=df["relative firing time"], data=df["IF"])
+            result, chisqr = self.lm_fit.fit_the_function(func_class=func_class, param_values=param_values,
+                                                    x=df["relative firing time"], data=df["IF"])
             final = func_class(params=result.params, x=df["relative firing time"])
             mean = np.mean(df["IF"])
             r_2 = (np.sum((mean-df["IF"]) ** 2) - np.sum((df["IF"] - final) ** 2)) / np.sum((mean-df["IF"]) ** 2)
